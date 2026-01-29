62e48efcbcd016db2b64ce401fa63782e3abf9cb
azizbekjonsanginov0606-cmd <azizbekjonsanginov0606@gmail.com>
2026-01-29 19:40:18 +0500

    d

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,9 +3,6 @@
 using task1;
 Cache<int>.Add(10);
 int value = Cache<int>.GetValue(10);
-Cache<int>.Add(20);
-value=Cache<int>.GetValue(20);
-Console.WriteLine(value);
 Cache<int>.RemoveValue(10);
 int valueg = Cache<int>.GetValue(value);
 Console.WriteLine(valueg);
